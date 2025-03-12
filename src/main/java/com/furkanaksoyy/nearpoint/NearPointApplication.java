820b1ce2f92acb1337989d3963e66c35f82894e6
FurkanAksoyy <furkanaksoy178@gmail.com>
2025-03-12 15:01:36 +0300

    Initial commit: Project setup with Spring Boot

---
+++ src/main/java/com/furkanaksoyy/nearpoint/NearPointApplication.java
@@ -0,0 +1,13 @@
+package com.furkanaksoyy.nearpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furkanaksoyy/nearpoint/NearPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanaksoyy.nearpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
