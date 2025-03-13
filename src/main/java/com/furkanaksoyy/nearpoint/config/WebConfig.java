b625f93f3648d3be0ac9c3f6942f85ffddd15352
FurkanAksoyy <furkanaksoy178@gmail.com>
2025-03-14 02:14:45 +0300

    Update WebConfig.java

--- src/main/java/com/furkanaksoyy/nearpoint/config/WebConfig.java
+++ src/main/java/com/furkanaksoyy/nearpoint/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Frontend URL
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001") // Frontend URL, Added 3001
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
